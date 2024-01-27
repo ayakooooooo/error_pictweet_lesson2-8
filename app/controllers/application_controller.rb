dc4e77a92596fbd84182506b2612ba46d486745d
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2024-01-27 20:02:46 +0900

    「first_name」と「last_name」がデータベースに保存できるようにコントローラーファイルを編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
+#「devise由来で作成したテーブルに追加したカラム」に保存する値は、そのままだと保存できない設定
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @nickname = user.nickname
     @tweets = user.tweets
+    
   end
 end
